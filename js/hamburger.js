a2b1a421914744664bcc734c54cbe5759ae6f371
Антон Ле Ван <antonlevan@Antons-MacBook-Air.local>
2023-02-03 23:16:29 +0700

    first coment

---
+++ js/hamburger.js
@@ -0,0 +1,28 @@
+const openElement = document.querySelector(".hamburger");
+const closeElement = document.querySelector(".cross__logo");
+const body = document.body;
+
+openElement.addEventListener("click", e => {
+	e.preventDefault();
+
+	var humburgerMenu = document.querySelector("#humburgerMenu")
+	humburgerMenu.classList.remove(".hamburger-menu")
+	
+	var hamburgerMain = document.querySelector(".hamburger-menu");
+	hamburgerMain.classList.toggle("hamburger-main");
+});
+
+closeElement.addEventListener("click", e => {
+	e.preventDefault();
+
+	var hamburgerMain = document.querySelector(".hamburger-menu");
+	hamburgerMain.classList.remove("hamburger-main");
+});
+
+
+humburgerMenu.addEventListener("click", e => {
+	if(!e.target.classList.contains("hamburger-menu__link")) {
+		closeElement.click();
+	}
+});
+
