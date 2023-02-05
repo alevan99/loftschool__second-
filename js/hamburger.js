e13b6081bf8cced78df93a749a3d2e4cec7a846d
Антон Ле Ван <antonlevan@Antons-MacBook-Air.local>
2023-02-05 20:37:50 +0700

    first coment

--- js/hamburger.js
+++ js/hamburger.js
@@ -28,16 +28,34 @@ humburgerMenu.addEventListener("click", e => {
 
 
 /////////////////////////////////////////////
-
-const interactiveLink = document.querySelector(".interactive-avatar");
-
-interactiveLink.addEventListener("click", (e) => {
-	e.preventDefault();
-})
+// REVIEWS
+/////////////////////////////////////////////
 
 
 
-// $(".interactive-avatar__link").click(e => {
+// const interactiveLink = document.querySelector(".interactive-avatar");
+
+// interactiveLink.addEventListener("click", (e) => {
 // 	e.preventDefault();
-
 // })
+
+const findBlockByAlias = alias => {
+	return $(".reviews__item").filter((ndx, item) => {
+		return $(item).attr("data-linked-with") == alias
+	});
+};
+
+$(".interactive-avatar__link").click((e) => {
+	e.preventDefault();
+
+	const $this = $(e.currentTarget);
+	const target = $this.attr("data-open");
+	const itemToShow = findBlockByAlias(target);
+	const curItem = $this .closest(".reviews__switcher-item");
+
+	itemToShow.addClass("active").siblings().removeClass("active");
+	curItem.addClass("active").siblings().removeClass("active");
+
+});
+
+
